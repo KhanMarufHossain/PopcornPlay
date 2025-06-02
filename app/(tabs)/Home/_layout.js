a3a645a5f5648edc0e1c4541bb4416d76fdcfd99
KhanMarufHossain <khan.hossain.242@northsouth.edu>
2025-06-02 17:11:32 +0600

    working on home layout

--- app/(tabs)/Home/_layout.js
+++ app/(tabs)/Home/_layout.js
@@ -4,9 +4,43 @@ import { Tabs } from 'expo-router'
 
 const _layout = () => {
   return (
-    <Tabs screenOptions={{tabBarPosition: 'top'}}>
-        <Tabs.Screen name= "moviesHome" />
-        <Tabs.Screen name ="tvShowsHome"/>
+    <Tabs 
+      screenOptions={{
+        tabBarPosition: 'top',
+        tabBarStyle: {
+          backgroundColor: '#1A1A2E',
+          elevation: 0,
+          shadowOpacity: 0,
+          borderBottomWidth: 0,
+          height: 120,
+        },
+        tabBarActiveTintColor: '#FF2E63',
+        tabBarInactiveTintColor: '#FFFFFF',
+        tabBarLabelStyle: {
+          fontSize: 14,
+          fontWeight: 'bold',
+          textTransform: 'uppercase',
+        },
+        tabBarIndicatorStyle: {
+          backgroundColor: '#FF2E63',
+          height: 3,
+        },
+      
+        headerShown: false, 
+      }}
+    >
+        <Tabs.Screen 
+          name="moviesHome" 
+          options={{
+            title: "Movies",
+          }}
+        />
+        <Tabs.Screen 
+          name="tvShowsHome"
+          options={{
+            title: "TV Shows",
+          }}
+        />
     </Tabs>
   )
 }
