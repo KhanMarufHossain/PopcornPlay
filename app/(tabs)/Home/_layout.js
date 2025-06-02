0cd31ba7fb8fd0398a7cac6cfc5c08a152aa5f87
KhanMarufHossain <khan.hossain.242@northsouth.edu>
2025-06-02 17:12:38 +0600

    added icons

--- app/(tabs)/Home/_layout.js
+++ app/(tabs)/Home/_layout.js
@@ -1,6 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
+import { Ionicons } from '@expo/vector-icons'
 
 const _layout = () => {
   return (
@@ -17,7 +18,7 @@ const _layout = () => {
         tabBarActiveTintColor: '#FF2E63',
         tabBarInactiveTintColor: '#FFFFFF',
         tabBarLabelStyle: {
-          fontSize: 14,
+          fontSize: 12,
           fontWeight: 'bold',
           textTransform: 'uppercase',
         },
@@ -25,20 +26,33 @@ const _layout = () => {
           backgroundColor: '#FF2E63',
           height: 3,
         },
-      
-        headerShown: false, 
+        headerShown: false,
       }}
     >
         <Tabs.Screen 
           name="moviesHome" 
           options={{
             title: "Movies",
+            tabBarIcon: ({ color, focused }) => (
+              <Ionicons 
+                name={focused ? "film" : "film-outline"} 
+                size={22} 
+                color={color} 
+              />
+            ),
           }}
         />
         <Tabs.Screen 
           name="tvShowsHome"
           options={{
             title: "TV Shows",
+            tabBarIcon: ({ color, focused }) => (
+              <Ionicons 
+                name={focused ? "tv" : "tv-outline"} 
+                size={22} 
+                color={color} 
+              />
+            ),
           }}
         />
     </Tabs>
