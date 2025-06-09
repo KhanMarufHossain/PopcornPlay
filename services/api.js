b14a1ff1edc9e6e6c709696164b6fa29d9741012
KhanMarufHossain <khan.hossain.242@northsouth.edu>
2025-06-09 17:35:23 +0600

    making an api

---
+++ services/api.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const TMDB_CONFIG = {
+  BASE_URL: "https://api.themoviedb.org/3/",
+
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${process.env.EXPO_PUBLIC_AUTH_TOKEN}`,
+  },
+};
+
+export const showMovies = async () => {
+  const endpoint = `${TMDB_CONFIG.BASE_URL}discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc`;
+
+  try {
+    const response = await axios.get(endpoint, {
+      headers: TMDB_CONFIG.headers,
+    });
+
+    return response.data.results;
+  } catch (error) {
+    const statusText =
+      error.response?.statusText || error.message || "Unknown Error";
+    throw new Error(`failed to fetch movies , error  : ${statusText}`);
+  }
+};
