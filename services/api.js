88c40d11baffcacbb1e71cd58a0217e82d0c2b06
KhanMarufHossain <khan.hossain.242@northsouth.edu>
2025-06-10 11:19:17 +0600

    adding paginations

--- services/api.js
+++ services/api.js
@@ -9,18 +9,23 @@ const TMDB_CONFIG = {
   },
 };
 
-export const showMovies = async () => {
-  const endpoint = `${TMDB_CONFIG.BASE_URL}discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc`;
+export const showMovies = async (page = 1) => {
+  const endpoint = `${TMDB_CONFIG.BASE_URL}discover/movie?include_adult=false&include_video=false&language=en-US&page=${page}&sort_by=popularity.desc`;
 
   try {
     const response = await axios.get(endpoint, {
       headers: TMDB_CONFIG.headers,
     });
 
-    return  {movies : response.data.results};
+    return {
+      movies: response.data.results,
+      currentPage: response.data.page,
+      totalPages: response.data.total_pages,
+      totalResults: response.data.total_results
+    };
   } catch (error) {
     const statusText =
       error.response?.statusText || error.message || "Unknown Error";
-    throw new Error(`failed to fetch movies , error  : ${statusText}`);
+    throw new Error(`failed to fetch movies, error: ${statusText}`);
   }
 };
